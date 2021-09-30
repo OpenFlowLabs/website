3a5f3c1c61d92cf82d213e895347ed270d8f25d1
Aziroshin <aziroshin@gmx.net>
2021-09-30 10:33:38 +0200

    🔨 Branch names can now be prefixed with `no-husky/`, making it so that husky won't run hooks when committing to them.

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -6,8 +6,23 @@ if [ -z "$husky_skip_init" ]; then
     fi
   }
 
+  branch_name=$(git rev-parse --abbrev-ref HEAD)
+  branch_prefix=${branch_name%/*}
   readonly hook_name="$(basename "$0")"
   debug "starting $hook_name..."
+
+  if [ "$branch_name" = "no-husky" ]; then
+      # This case has too much potential to be irritating to only
+      # be shown when $HUSKY_DEBUG = "1", hence `printf`.
+      printf "Branch name is exactly \"no-husky\", but \"no-husky\" is "
+      printf "only allowed as a prefix. \e[35mAborting\e[0m.\n"
+      exit 1
+  fi
+
+  if [ "$branch_prefix" = "no-husky" ]; then
+    debug "Branch name starts with no-husky/, skipping hook"
+    export HUSKY=0
+  fi
 
   if [ "$HUSKY" = "0" ]; then
     debug "HUSKY env variable is set to 0, skipping hook"
