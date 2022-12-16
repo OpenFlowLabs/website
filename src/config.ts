8341ad77664d702716975bfccd257d849b1f64f8
Till Wegmueller <toasterson@gmail.com>
2022-12-16 15:26:17 +0100

    New Website based on Astro

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Index | OFLabs';
+export const SITE_DESCRIPTION = 'Find out about all our services here!';
---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly PUBLIC_MAPBOX_ACCESS_TOKEN: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
